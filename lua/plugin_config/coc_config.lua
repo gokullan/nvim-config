3e3c1aa23427a3d57116dff558b5e0fd376f960d
Gokulakrishnan S <gokullan1101@gmail.com>
2025-05-17 23:41:40 +0530

    feat: Add coc-function-definiton preview

--- lua/plugin_config/coc_config.lua
+++ lua/plugin_config/coc_config.lua
@@ -32,3 +32,18 @@ end, opts)
 
 -- Code navigation
 keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
+
+-- Use K to show documentation in preview window
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+keyset("n", "k", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+
+-- Reference: https://github.com/neoclide/coc.nvim/discussions/3776#discussioncomment-5463741
