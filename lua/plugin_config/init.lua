219a28275b4beef8b5a899cb7cc891f47662e51e
Gokulakrishnan S <gokullan1101@gmail.com>
2025-05-17 23:50:17 +0530

    feat: Enable telescope

--- lua/plugin_config/init.lua
+++ lua/plugin_config/init.lua
@@ -5,4 +5,4 @@ require('plugin_config/dap_config')
 -- coc
 require('plugin_config/coc_config')
 -- telescope
--- require('plugin_config/telescope_config')
+require('plugin_config/telescope_config')
